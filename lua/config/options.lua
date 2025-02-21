abb369613988429c7d0d86b2420ea3913fe74085
Yilin Fang <qzfyl98@outlook.com>
2025-02-21 09:04:05 -0500

    Set defualt clipboard to OSC 52 to support Zellij

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable relative numbers
 vim.opt.number = true
 vim.opt.relativenumber = false
+
+-- Set the OSC 52 as default clipboard
+-- For Zellij compatibility, if you use tmux, you can remove this setting as neovim will use the system clipboard
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
