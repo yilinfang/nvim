1851180070c821c363ad4300381b8fee63177acb
Yilin Fang <qzfyl98@outlook.com>
2025-03-18 16:13:47 -0400

    Update configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.number = true
-vim.opt.relativenumber = false
-
 vim.opt.scrolloff = 10
 vim.opt.clipboard = ""
 
