fbbfd14ee7be35a536216262f1c9e09021a7312a
Yilin Fang <qzfyl98@outlook.com>
2025-03-20 11:11:44 -0400

    Revert to snacks

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,26 +1,26 @@
 return {
---   {
---     "saghen/blink.cmp",
---     opts = {
---       completion = {
---         -- https://cmp.saghen.dev/configuration/completion.html#list
---         list = {
---           selection = {
---             preselect = false,
---             auto_insert = false,
---           },
---         },
---         trigger = {
---           -- https://cmp.saghen.dev/configuration/keymap.html#super-tab
---           show_in_snippet = false,
---         },
---       },
---       -- https://cmp.saghen.dev/configuration/keymap.html#super-tab
---       keymap = {
---         preset = "super-tab",
---         ["<Right>"] = { "accept", "fallback" },
---         ["<Enter>"] = { "accept", "fallback" },
---       },
---     },
---   },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        -- https://cmp.saghen.dev/configuration/completion.html#list
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+        trigger = {
+          -- https://cmp.saghen.dev/configuration/keymap.html#super-tab
+          show_in_snippet = false,
+        },
+      },
+      -- https://cmp.saghen.dev/configuration/keymap.html#super-tab
+      keymap = {
+        preset = "super-tab",
+        ["<Right>"] = { "accept", "fallback" },
+        ["<Enter>"] = { "accept", "fallback" },
+      },
+    },
+  },
 }
