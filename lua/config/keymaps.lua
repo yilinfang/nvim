1593d057fed8e9af3d1019646e67daf07af417cb
Yilin Fang <qzfyl98@outlook.com>
2025-03-24 23:19:29 -0400

    Update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Select all
-vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Selcet all", noremap = true })
-
 -- Copy to system clipboard +
-vim.keymap.set({ "v" }, "<C-c>", '"+y', { desc = "Copy to system clipboard '\"='" })
+vim.keymap.set({ "v" }, "ys", '"+y', { desc = "Copy to system clipboard '\"='" })
