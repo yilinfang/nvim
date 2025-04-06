396dfed0f00f35ea658fb80193cea27f0f944351
Yilin Fang <qzfyl98@outlook.com>
2025-04-06 11:15:28 -0400

    Update keymaps

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,9 +2,9 @@ return {
   "supermaven-inc/supermaven-nvim",
   opts = {
     keymaps = {
-      accept_suggestion = "<M-S-y>",
-      clear_suggestion = "<M-S-c>",
-      accept_word = "<M-S-w>",
+      accept_suggestion = "<M-y>",
+      clear_suggestion = "<M-c>",
+      accept_word = "<M-w>",
     },
   },
 }
