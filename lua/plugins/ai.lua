b86a8e518824516b2516c265f6251df2898ac3c0
Yilin Fang <qzfyl98@outlook.com>
2025-04-06 13:00:36 -0400

    Update plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,4 +1,18 @@
 return {
+  -- "zbirenbaum/copilot.lua",
+  -- opts = {
+  --   copilot_model = "gpt-4o-copilot",
+  --   suggestion = {
+  --     keymap = {
+  --       accept = "<M-S-y>",
+  --       accept_word = "<M-S-w>",
+  --       accept_line = "<M-S-l>",
+  --       next = "<M-S-n>",
+  --       prev = "<M-S-p>",
+  --       dismiss = "<M-S-d>",
+  --     },
+  --   },
+  -- },
   "supermaven-inc/supermaven-nvim",
   opts = {
     keymaps = {
