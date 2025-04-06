edcece1e4880b1e170fac9269ac9f2a2d4fffa80
Yilin Fang <qzfyl98@outlook.com>
2025-04-06 10:44:06 -0400

    Switch copilot to supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,18 +1,10 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      copilot_model = "gpt-4o-copilot",
-      suggestion = {
-        keymap = {
-          accept = "<M-S-y>",
-          accept_word = "<M-S-w>",
-          accept_line = "<M-S-l>",
-          next = "<M-S-n>",
-          prev = "<M-S-p>",
-          dismiss = "<M-S-d>",
-        },
-      },
+  "supermaven-inc/supermaven-nvim",
+  opts = {
+    keymaps = {
+      accept_suggestion = "<M-S-y>",
+      clear_suggestion = "<M-S-c>",
+      accept_word = "<M-S-w>",
     },
   },
 }
