3bc0e7eb96dc79abe676d7a6b4cc996507904113
Yilin Fang <qzfyl98@outlook.com>
2025-04-07 23:54:56 -0400

    Use self-configured copilot instead of extra

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -15,13 +15,39 @@ return {
   --     },
   --   },
   -- },
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   opts = {
+  --     keymaps = {
+  --       accept_suggestion = "<M-y>",
+  --       clear_suggestion = "<M-c>",
+  --       accept_word = "<M-w>",
+  --     },
+  --   },
+  -- },
   {
-    "supermaven-inc/supermaven-nvim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
     opts = {
-      keymaps = {
-        accept_suggestion = "<M-y>",
-        clear_suggestion = "<M-c>",
-        accept_word = "<M-w>",
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = "<M-y>",
+          accept_word = "<M-w>",
+          accept_line = "<M-l>",
+          next = "<M-n>",
+          prev = "<M-p>",
+          dismiss = "<M-d>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
       },
     },
   },
