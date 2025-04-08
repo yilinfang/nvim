6ce24445a4151a0b880504561428fe78b98400bc
Yilin Fang <qzfyl98@outlook.com>
2025-04-08 01:51:26 -0400

    Update colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- Add solarzied-osaka
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = false,
+    },
+  },
+  -- Configure Lazyvim to load solarized-osaka
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
