c075839caaf70748726920e23a9d21cfefa03dcf
Yilin Fang <qzfyl98@outlook.com>
2025-04-15 18:09:45 -0400

    Switch to solarized-osaka

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 return {
-  { -- Make Tokyo Night transparent
-    "folke/tokyonight.nvim",
+  -- Add solarzied-osaka
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
     opts = {
-      style = "night",
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
+    },
+  },
+  -- Configure Lazyvim to load solarized-osaka
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
     },
   },
 }
