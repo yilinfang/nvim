e619b29a9a62a4b680d1d25771b6699210fba268
Yilin Fang <qzfyl98@outlook.com>
2025-05-13 20:13:35 -0400

    Replace mini.diff with gitsigns

--- init.lua
+++ init.lua
@@ -159,13 +159,76 @@ rtp:prepend(lazypath)
 -- [[ Configure and install plugins ]]
 require('lazy').setup({
   { -- Detect tabstop and shiftwidth automatically
-    'NMAC427/guess-indent.nvim',
+    'NMAC427/guess-indent.nvim', -- HACK: guess-indent.nvim
     config = function()
       require('guess-indent').setup {}
       vim.keymap.set('n', '<leader>g', function()
         vim.cmd 'GuessIndent'
       end, { desc = '[G]uess Indent' })
     end,
+  },
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr) -- HACK: Additional keymaps for gitsigns
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end, { desc = 'Jump to next git [c]hange' })
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end, { desc = 'Jump to previous git [c]hange' })
+
+        -- Actions
+        -- visual mode
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [s]tage hunk' })
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'git [r]eset hunk' })
+        -- normal mode
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>hD', function()
+          gitsigns.diffthis '@'
+        end, { desc = 'git [D]iff against last commit' })
+        -- Toggles
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+      end,
+    },
   },
 
   { -- Useful plugin to show you pending keybinds.
@@ -683,7 +746,7 @@ require('lazy').setup({
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('todo-comments').setup { signs = true }
+      require('todo-comments').setup { signs = false }
       -- Set keymaps
       vim.keymap.set('n', '<leader>st', function()
         Snacks.picker.todo_comments()
@@ -702,16 +765,6 @@ require('lazy').setup({
 
       -- HACK: Other Mini modules
       require('mini.comment').setup()
-      require('mini.diff').setup {
-        view = {
-          style = 'sign',
-          signs = {
-            add = '▎',
-            change = '▎',
-            delete = '',
-          },
-        },
-      }
       require('mini.icons').setup()
       require('mini.move').setup()
       require('mini.pairs').setup()
